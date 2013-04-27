737c2c721221268d619e07b14bf2c0004e7ae1dd
filareta <filareta_yordanova@abv.bg>
2013-04-28 00:23:44 +0300

    Modify cache_func and zip_with

--- task2/solution.py
+++ task2/solution.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from collections import OrderedDict
+from collections import deque
 
 
 def groupby(func, seq):
@@ -23,26 +23,62 @@ def iterate(func):
         composition = composer(func, composition)
 
 
-def length(iterable):
-    for index, item in enumerate(iterable):
-        length_ = index + 1
-    return length_
-
-
 def zip_with(func, *iterables):
-    if not all([item is None for item in iterables]):
-        min_length = min(map(length, iterables))
-        for i in range(min_length):
-            yield func(*(map(lambda k: k[i], iterables)))
+    for args in zip(*iterables):
+        yield func(*args)
 
 
 def cache(func, cache_size):
+    cache = deque(maxlen=cache_size)
+
     def func_cached(*args):
-        if not hasattr(func, '_cache'):
-            func._cache = OrderedDict()
-        if len(func._cache) > cache_size:
-            func._cache.popitem(False)
-        if not args in func._cache:
-            func._cache[args] = func(*args)
-        return func._cache[args]
+        for cache_args, cache_value in cache:
+            if cache_args == args:
+                return cache_value
+        next_cache = func(*args)
+        cache.append((args, next_cache))
+        return next_cache
+
+    return func_cached
+from collections import defaultdict
+from collections import deque
+
+
+def groupby(func, seq):
+    keys = map(func, seq)
+    group_values = defaultdict(list)
+    iterator = iter(keys)
+    for item in seq:
+        group_values[next(iterator)].append(item)
+    return group_values
+
+
+def composer(f, g):
+    return lambda x: f(g(x))
+
+
+def iterate(func):
+    yield lambda x: x
+    composition = func
+    while True:
+        yield composition
+        composition = composer(func, composition)
+
+
+def zip_with(func, *iterables):
+    for args in zip(*iterables):
+        yield func(*args)
+
+
+def cache(func, cache_size):
+    cache = deque(maxlen=cache_size)
+
+    def func_cached(*args):
+        for cache_args, cache_value in cache:
+            if cache_args == args:
+                return cache_value
+        next_cache = func(*args)
+        cache.append((args, next_cache))
+        return next_cache
+
     return func_cached
